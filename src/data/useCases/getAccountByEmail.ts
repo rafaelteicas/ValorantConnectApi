d8668d45512e98a5c30ee447f1e0a39b5fa7ab2f
rafaelteicas <rafaelteicas@gmail.com>
2023-11-18 18:19:20 -0300

    feat: create GetAccountByEmail

---
+++ src/data/useCases/getAccountByEmail.ts
@@ -0,0 +1,17 @@
+import { type User } from '../../infra/typeorm/entities/User'
+import { type UserRepository } from '../../infra/typeorm/repositories/userRepository'
+
+export class GetAccountByEmail {
+  private readonly userRepository: typeof UserRepository
+
+  constructor (userRepository: typeof UserRepository) {
+    this.userRepository = userRepository
+  }
+
+  async get (email: string): Promise<User> {
+    if (!email) throw new Error()
+    const user = await this.userRepository.findOneBy({ email })
+    if (!user) throw new Error('USER NOT FOUND')
+    return user
+  }
+}
