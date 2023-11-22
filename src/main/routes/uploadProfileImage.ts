eef7b538eac1904be7dfa9a0184408347337d110
Rafael <rafaelteicas@gmail.com>
2023-11-22 17:31:06 -0300

    refactor: add authMiddleware to route

--- src/main/routes/uploadProfileImage.ts
+++ src/main/routes/uploadProfileImage.ts
@@ -2,7 +2,8 @@ import { type Router } from 'express'
 import { adapterExpress } from '../adapter/express'
 import { makeUploadProfileImage } from '../factories/upload/makeUploadProfileImage'
 import { storage } from '../../infra/storage/multer'
+import { authMiddleware } from '../middlewares/authMiddleware'
 
 export default function (route: Router): void {
-  route.post('/profileImage/:id', storage.single('image'), adapterExpress(makeUploadProfileImage()))
+  route.post('/profileImage/:id', storage.single('image'), authMiddleware, adapterExpress(makeUploadProfileImage()))
 }
