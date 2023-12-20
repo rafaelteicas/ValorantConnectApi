09540512bb880ac13cb06338d5f8a2f871257bbe
Rafael <rafaelteicas@gmail.com>
2023-12-20 17:11:49 -0300

    refactor: create function to get DataSource

--- src/infra/typeorm/dataSource.ts
+++ src/infra/typeorm/dataSource.ts
@@ -5,16 +5,26 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const isProd = (): any => {
+  if (process.env.DATABASE_URL) {
+    return {
+      ssl: {
+        rejectUnauthorized: true
+      },
+      url: process.env.DATABASE_URL
+    }
+  }
+    return {
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_DATABASE,
+      password:  process.env.DB_PASSWORD
+    }
+}
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   port: 5432,
   synchronize: true,
   entities: [path.join(__dirname, '/entities/*.{js,ts}')],
-  // ssl: {
-  //   rejectUnauthorized: true
-  // },
-  url: process.env.DATABASE_URL,
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USERNAME,
-  password:  process.env.DB_PASSWORD
+  ...isProd()
 })
